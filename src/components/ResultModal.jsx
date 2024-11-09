307c51807621d7fcc73f9f63ada42381eb500fd5
Obaida Alzaben <obaidahatem100@gmail.com>
2024-11-09 09:03:28 +0300

    Finish

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,15 +1,24 @@
 
-import { forwardRef } from "react"
-const ResultModal = forwardRef(function ResultModal ({ result, targetTime }, ref){
-    return(
+import { forwardRef } from "react";
+import { createPortal } from 'react-dom';
+
+const ResultModal = forwardRef(function ResultModal ({ targetTime, remainigTime, onReset }, ref){
+
+    const userLost = remainigTime <= 0;
+    const formattedRemainingTime = (remainigTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainigTime /(targetTime * 1000))* 100);
+
+    return createPortal(
         <dialog ref={ref} className="result-modal" >
-            <h2>You {result}</h2>
+            {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     )
 })
 export default ResultModal;
